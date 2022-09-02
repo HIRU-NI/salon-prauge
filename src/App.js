4fdfd657219b3705b554056a3d7469c70e7072ff
Hiruni <hiruni@zynch.com>
2022-09-02 15:20:37 +0530

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Header from "./components/home/Header"
-import Home from "./pages/Home/index.js"
+import Home from "./pages/Home"
 
 import { useRef } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "slick-carousel/slick/slick.css"
@@ -13,8 +14,31 @@ function App() {
 
   return (
     <>
-      <Header aboutRef={aboutRef} servicesRef={servicesRef} />
-      <Home aboutRef={aboutRef} servicesRef={servicesRef} />
+      <Router>
+        <Header aboutRef={aboutRef} servicesRef={servicesRef} />
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav> */}
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route
+            path="/"
+            element={<Home aboutRef={aboutRef} servicesRef={servicesRef} />}
+          ></Route>
+        </Routes>
+      </Router>
     </>
   )
 }
